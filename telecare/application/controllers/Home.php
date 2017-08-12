4fb6615ffe453f0ac00a418add29867b00ebc01c
calinblitiu <calin.blitiu@outlook.com>
2017-08-13 05:26:48 +0800

    layout header create

--- telecare/application/controllers/Home.php
+++ telecare/application/controllers/Home.php
@@ -14,6 +14,7 @@ class Home extends CI_Controller {
 
 	public function signUpDoctor()
 	{
+		$this->load->view('layout/header');
 		$this->load->view('doctor/signup');
 	}
 }
---
+++ telecare/application/views/doctor/signup.php
@@ -0,0 +1,43 @@
+<div class="row">
+    <div class="col-md-6">
+      <div class="portlet box blue ">
+        <div class="portlet-title">
+          <div class="caption">
+            <i class="fa fa-gift"></i> Sign Up Doctor
+          </div>
+          <div class="tools">
+            <a href="" class="collapse" data-original-title="" title="">
+            </a>
+            <a href="#portlet-config" data-toggle="modal" class="config" data-original-title="" title="">
+            </a>
+            <a href="" class="reload" data-original-title="" title="">
+            </a>
+            <a href="" class="remove" data-original-title="" title="">
+            </a>
+          </div>
+        </div>
+        <div class="portlet-body form">
+          <form role="form">
+            <div class="form-body">
+              <div class="form-group has-success">
+                <label class="control-label">Input with success</label>
+                <input type="text" class="form-control" id="inputSuccess">
+              </div>
+              <div class="form-group has-warning">
+                <label class="control-label">Input with warning</label>
+                <input type="text" class="form-control" id="inputWarning">
+              </div>
+              <div class="form-group has-error">
+                <label class="control-label">Input with error</label>
+                <input type="text" class="form-control" id="inputError">
+              </div>
+            </div>
+            <div class="form-actions">
+              <button type="button" class="btn default">Cancel</button>
+              <button type="submit" class="btn red">Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+</div>
