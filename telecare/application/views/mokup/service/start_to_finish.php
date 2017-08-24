09da71c3fe3a1b770936c9f95f7953da144d8575
calinblitiu <calin.blitiu@outlook.com>
2017-08-25 05:36:47 +0800

    mokup start to service

--- telecare/application/views/mokup/service/start_to_finish.php
+++ telecare/application/views/mokup/service/start_to_finish.php
@@ -7,14 +7,40 @@
  */?>
 <html>
 <head>
-    <title>Moke Up | Start To Finish</title>
-    <link href="<?=base_url()?>assets/mokup/mokup.css">
+    <title>Moke Up | Start To Finish </title>
+    <link rel="stylesheet" href="<?=base_url()?>assets/mokup/mokup.css">
+    <link rel="stylesheet" href="<?=base_url()?>assets/global/plugins/bootstrap/css/bootstrap.min.css">
+    <script src="<?=base_url()?>assets/global/plugins/jquery.min.js"></script>
+    <script src="<?=base_url()?>assets/global/plugins/bootstrap/js/bootstrap.js"></script>
     <script src="<?=base_url()?>assets/mokup/mokup.js"></script>
+
 </head>
 
 <body>
-<h1>Start To Finish</h1>
+
+<div class="row mokup-border" style="height: 100px; margin: 50px 10% 0 10%;text-align: center;">
+    Header
+</div>
+<div class="row" style="height: 300px; margin: 50px 10%;">
+    <img class="mokup-no-imag" src="<?=base_url()?>assets/mokup/noimage.png">
+</div>
+<div class="row" style="height: 80%; margin: 50px 10%; padding: 0 5%;">
+    <div class="col-md-8 mokup-border" style="height: 100%;text-align: ">
+
+        <span style="position: absolute;top: 30%;text-align: center">Start to Finish Consultation.<br>
+            An infectious disease consultation will be initiated and the physician will do regular follow up until the conclusion of the treatment. This may involves weekly face to face and daily text messaging service.</span>
+
+    </div>
+
+    <div class="col-md-4"  style="height: 100%;padding: 0;text-align: center;">
+       <a class="mokup-border" style="padding: 10px 30px; position: absolute;top: 30%;">Request a Consultation</a>
+    </div>
+</div>
+
+
+<div class="row mokup-border" style="height: 100px; margin: 0 10% 50px 10%;text-align: center;">
+    footer
+</div>
+
+
 </body>
-</html>
-
-
