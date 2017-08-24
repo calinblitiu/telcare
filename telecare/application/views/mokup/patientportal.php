568e3fb9b7b8dbd67046b653f164a0365a1a414a
calinblitiu <calin.blitiu@outlook.com>
2017-08-24 11:25:55 +0800

    provider portal

--- telecare/application/views/mokup/patientportal.php
+++ telecare/application/views/mokup/patientportal.php
@@ -18,4 +18,39 @@
 
 <body>
 
-<div class="row mokup-border" style="height: 600px; margin: 0 10%;">
+<div class="row mokup-border" style="height: 100px; margin: 50px 10% 0 10%;">
+</div>
+<div class="row" style="height: 300px; margin: 50px 10%;">
+
+    <div class="col-md-10" style="height: 100%;padding: 0;">
+        <img class="mokup-no-imag" src="<?=base_url()?>assets/mokup/noimage.png">
+        <button class="mokup-border-round" style="position: absolute;top: 50%;left: 15%;">Get Started</button>
+    </div>
+    <div class="col-md-2" style="height: 100%;padding: 0;">
+        <img class="mokup-no-imag" src="<?=base_url()?>assets/mokup/noimage.png">
+        <span class="mokup-border" style="position: absolute;left:20%;top: 20%;">Tele-ID consult image</span>
+    </div>
+
+</div>
+<div class="row" style="height: 400px; margin: 50px 10%; padding: 0 10%;">
+
+    <div class="col-md-5 mokup-border" style="height: 100%;padding: 0;text-align: center;">
+        <div style="position: absolute;top: 50%;padding: 0 20%;">
+        <span style="">Tele-Infectious Disease Consultation your  own personal ID Doctor</span>
+        <span>Sign up</span>
+        </div>
+    </div>
+    <div class="col-md-5 col-md-offset-2 mokup-border" style="height: 100%;padding: 0;text-align: center;vertical-align: middle;">
+        <div style="position: absolute;top: 50%;text-align: center;width: 100%;">
+        <span>What we treat</span><br><br><br><br><br><br>
+        <button class="mokup-border-round">Sign Up</button>
+        </div>
+    </div>
+
+</div>
+
+<div class="row mokup-border" style="height: 100px; margin: 0 10% 50px 10%;">
+</div>
+
+
+</body>
