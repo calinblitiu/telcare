61fe25cea86920c30467bdc71237e5c6eb903f83
calinblitiu <calin.blitiu@outlook.com>
2017-08-24 11:33:16 +0800

    mokup provider portal

--- telecare/application/views/mokup/provider_portal/providerportal.php
+++ telecare/application/views/mokup/provider_portal/providerportal.php
@@ -7,13 +7,48 @@
  */?>
 <html>
 <head>
-    <title>Moke Up | Provider Portal</title>
-    <link href="<?=base_url()?>assets/mokup/mokup.css">
+    <title>Moke Up | Patient Porta</title>
+    <link rel="stylesheet" href="<?=base_url()?>assets/mokup/mokup.css">
+    <link rel="stylesheet" href="<?=base_url()?>assets/global/plugins/bootstrap/css/bootstrap.min.css">
+    <script src="<?=base_url()?>assets/global/plugins/jquery.min.js"></script>
+    <script src="<?=base_url()?>assets/global/plugins/bootstrap/js/bootstrap.js"></script>
     <script src="<?=base_url()?>assets/mokup/mokup.js"></script>
+
 </head>
 
 <body>
-<h1>Provider Portal</h1>
-</body>
-</html>
 
+<div class="row mokup-border" style="height: 100px; margin: 50px 10% 0 10%;text-align: center">
+    Header
+</div>
+<div class="row" style="height: 300px; margin: 50px 10%;">
+    <img class="mokup-no-imag" src="<?=base_url()?>assets/mokup/noimage.png">
+
+</div>
+<div class="row" style="height: 300px; margin: 50px 10%; padding: 0 10%;">
+
+    <div class="col-md-3 mokup-border" style="height: 100%;padding: 0;text-align: center;">
+        <div style="position: absolute;top: 50%;padding: 0 20%;">
+            <a href="<?=base_url()?>">infectious disease consultant</a>
+        </div>
+    </div>
+    <div class="col-md-3 col-md-offset-1 mokup-border" style="height: 100%;padding: 0;text-align: center;">
+        <div style="position: absolute;top: 50%;text-align: center;width: 100%;">
+            <a href="<?=base_url()?>">Refering provider</a>
+        </div>
+    </div>
+
+    <div class="col-md-3 col-md-offset-1 mokup-border" style="height: 100%;padding: 0;text-align: center;">
+        <div style="position: absolute;top: 50%;text-align: center;width: 100%;">
+            <a href="<?=base_url()?>">Hospital</a>
+        </div>
+    </div>
+
+</div>
+
+<div class="row mokup-border" style="height: 100px; margin: 0 10% 50px 10%;text-align: center;">
+    footer
+</div>
+
+
+</body>
