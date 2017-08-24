5f94191a5e8afb93bc4da739c3277f2b67d64010
calinblitiu <calin.blitiu@outlook.com>
2017-08-25 00:16:20 +0800

    mokup Provider Registeration

--- telecare/application/views/mokup/provider_portal/registeration_provider.php
+++ telecare/application/views/mokup/provider_portal/registeration_provider.php
@@ -7,12 +7,43 @@
  */?>
 <html>
 <head>
-    <title>Moke Up | Registeration Provider</title>
-    <link href="<?=base_url()?>assets/mokup/mokup.css">
+    <title>Moke Up |  Provider Registeration</title>
+    <link rel="stylesheet" href="<?=base_url()?>assets/mokup/mokup.css">
+    <link rel="stylesheet" href="<?=base_url()?>assets/global/plugins/bootstrap/css/bootstrap.min.css">
+    <script src="<?=base_url()?>assets/global/plugins/jquery.min.js"></script>
+    <script src="<?=base_url()?>assets/global/plugins/bootstrap/js/bootstrap.js"></script>
     <script src="<?=base_url()?>assets/mokup/mokup.js"></script>
+
 </head>
 
 <body>
-<h1>Provider Registeration</h1>
+
+<div class="row mokup-border" style="height: 100px; margin: 50px 10% 0 10%;text-align: center;">
+    Header
+</div>
+
+
+<div class="row" style=" margin: 50px 10%; padding: 0 5%;text-align: center;">
+
+    <span class="mokup-border" style="font-size: 15px;padding: 5px;">Provider Registeration</span><br><br>
+    <span class="mokup-border" style="font-size: 15px;padding: 10px;">Tele-ID will contact you within 24hours with login/password information</span><br>
+    <input type="text" class="mokup-border" style="padding: 10px;width: 100%;margin: 30px 0;" placeholder="First Name">
+    <input type="text" class="mokup-border" style="padding: 10px;width: 100%;margin: 30px 0;" placeholder="Last Name">
+    <input type="email" class="mokup-border" style="padding: 10px;width: 100%;margin: 30px 0;" placeholder="Email Address">
+    <input type="text" class="mokup-border" style="padding: 10px;width: 100%;margin: 30px 0;" placeholder="Office Phone">
+    <input type="text" class="mokup-border" style="padding: 10px;width: 100%;margin: 30px 0;" placeholder="Office Fax">
+    <input type="text" class="mokup-border" style="padding: 10px;width: 100%;margin: 30px 0;" placeholder="NPI">
+    <input type="text" class="mokup-border" style="padding: 10px;width: 100%;margin: 30px 0;" placeholder="State Licence Number">
+    <input type="text" class="mokup-border" style="padding: 10px;width: 100%;margin: 30px 0;" placeholder="DEA">
+    <input type="text" class="mokup-border" style="padding: 10px;width: 100%;margin: 30px 0;" placeholder="CV Upload">
+
+    <button class="mokup-border-round">Send Request</button>
+
+</div>
+
+<div class="row mokup-border" style="height: 100px; margin: 0 10% 50px 10%;text-align: center;">
+    footer
+</div>
+
+
 </body>
-</html>
