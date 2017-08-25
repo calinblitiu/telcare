d2b0d871a8b088e8390330ccd539a1576877bc4f
calinblitiu <calin.blitiu@outlook.com>
2017-08-25 10:36:32 +0800

    mokup footer directory

--- telecare/application/views/mokup/footer/directory.php
+++ telecare/application/views/mokup/footer/directory.php
@@ -7,12 +7,59 @@
  */?>
 <html>
 <head>
-    <title>Moke Up | Directory</title>
-    <link href="<?=base_url()?>assets/mokup/mokup.css">
+    <title>Moke Up |  </title>
+    <link rel="stylesheet" href="<?=base_url()?>assets/mokup/mokup.css">
+    <link rel="stylesheet" href="<?=base_url()?>assets/global/plugins/bootstrap/css/bootstrap.min.css">
+    <script src="<?=base_url()?>assets/global/plugins/jquery.min.js"></script>
+    <script src="<?=base_url()?>assets/global/plugins/bootstrap/js/bootstrap.js"></script>
     <script src="<?=base_url()?>assets/mokup/mokup.js"></script>
+
 </head>
 
 <body>
-<h1>Directory</h1>
+
+<div class="row mokup-border" style="height: 100px; margin: 50px 10% 0 10%;text-align: center;">
+    Header
+</div>
+<div class="row" style="margin: 50px 10%;text-align: center">
+    <span class="mokup-border">Our Team</span>
+</div>
+<div class="row" style="margin: 50px 10%;padding: 0 10%">
+    <img class="mokup-no-imag" src="<?=base_url()?>assets/mokup/noimage.png" style="width: 40%;height: 200px;">
+    <img class="mokup-no-imag" src="<?=base_url()?>assets/mokup/noimage.png" style="width: 40%;height: 200px;margin-left: 10%">
+    <br><br>
+    <img class="mokup-no-imag" src="<?=base_url()?>assets/mokup/noimage.png" style="width: 40%;height: 200px;">
+    <img class="mokup-no-imag" src="<?=base_url()?>assets/mokup/noimage.png" style="width: 40%;height: 200px;margin-left: 10%">
+</div>
+
+<div class="row" style="margin: 50px 10%;text-align: center">
+    <span class="mokup-border">Advisory Board Members</span>
+</div>
+
+<div class="row" style="margin: 50px 10%;text-align: center">
+    <div class="col-md-2" style="height: 200px;">
+        <img class="mokup-no-imag" src="<?=base_url()?>assets/mokup/noimage.png">
+    </div>
+    <div class="col-md-2" style="height: 200px;">
+        <img class="mokup-no-imag" src="<?=base_url()?>assets/mokup/noimage.png">
+    </div>
+    <div class="col-md-2" style="height: 200px;">
+        <img class="mokup-no-imag" src="<?=base_url()?>assets/mokup/noimage.png">
+    </div>
+    <div class="col-md-2" style="height: 200px;">
+        <img class="mokup-no-imag" src="<?=base_url()?>assets/mokup/noimage.png">
+    </div>
+    <div class="col-md-2" style="height: 200px;">
+        <img class="mokup-no-imag" src="<?=base_url()?>assets/mokup/noimage.png">
+    </div>
+    <div class="col-md-2" style="height: 200px;">
+        <img class="mokup-no-imag" src="<?=base_url()?>assets/mokup/noimage.png">
+    </div>
+
+</div>
+<div class="row mokup-border" style="height: 100px; margin: 0 10% 50px 10%;text-align: center;">
+    footer
+</div>
+
+
 </body>
-</html>
