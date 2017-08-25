bbdc917362d425e6fe5b60afbde45d7ee547ebff
calinblitiu <calin.blitiu@outlook.com>
2017-08-25 10:42:17 +0800

    mokup terms and condition

--- telecare/application/views/mokup/footer/directory.php
+++ telecare/application/views/mokup/footer/directory.php
@@ -7,7 +7,7 @@
  */?>
 <html>
 <head>
-    <title>Moke Up |  </title>
+    <title>Moke Up |  Directory</title>
     <link rel="stylesheet" href="<?=base_url()?>assets/mokup/mokup.css">
     <link rel="stylesheet" href="<?=base_url()?>assets/global/plugins/bootstrap/css/bootstrap.min.css">
     <script src="<?=base_url()?>assets/global/plugins/jquery.min.js"></script>
--- telecare/application/views/mokup/terms_and_condition.php
+++ telecare/application/views/mokup/terms_and_condition.php
@@ -7,15 +7,27 @@
  */?>
 <html>
 <head>
-    <title>Moke Up | Terms And Condition</title>
-    <link href="<?=base_url()?>assets/mokup/mokup.css">
+    <title>Moke Up | Terms & Condition</title>
+    <link rel="stylesheet" href="<?=base_url()?>assets/mokup/mokup.css">
+    <link rel="stylesheet" href="<?=base_url()?>assets/global/plugins/bootstrap/css/bootstrap.min.css">
+    <script src="<?=base_url()?>assets/global/plugins/jquery.min.js"></script>
+    <script src="<?=base_url()?>assets/global/plugins/bootstrap/js/bootstrap.js"></script>
     <script src="<?=base_url()?>assets/mokup/mokup.js"></script>
+
 </head>
 
 <body>
-<h1>Terms And Condition</h1>
+
+<div class="row mokup-border" style="height: 100px; margin: 50px 10% 0 10%;text-align: center;">
+    Header
+</div>
+<div class="row" style="height: 600px; margin: 50px 10%;text-align: center;">
+    <span class="mokup-border" style="padding: 50px 20px;position: absolute;top: 50%;">Terms and Condition</span>
+</div>
+
+<div class="row mokup-border" style="height: 100px; margin: 0 10% 50px 10%;text-align: center;">
+    footer
+</div>
+
+
 </body>
-</html>
-
-
-
