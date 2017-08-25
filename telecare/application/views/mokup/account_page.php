1c80cb3d0a26d6b7f1ed2113b09c1e8f85abd8ea
calinblitiu <calin.blitiu@outlook.com>
2017-08-25 14:36:49 +0800

    mokup accounts page

--- telecare/application/views/mokup/account_page.php
+++ telecare/application/views/mokup/account_page.php
@@ -8,11 +8,47 @@
 <html>
 <head>
     <title>Moke Up | Accounts Page</title>
-    <link href="<?=base_url()?>assets/mokup/mokup.css">
+    <link rel="stylesheet" href="<?=base_url()?>assets/mokup/mokup.css">
+    <link rel="stylesheet" href="<?=base_url()?>assets/global/plugins/bootstrap/css/bootstrap.min.css">
+    <script src="<?=base_url()?>assets/global/plugins/jquery.min.js"></script>
+    <script src="<?=base_url()?>assets/global/plugins/bootstrap/js/bootstrap.js"></script>
     <script src="<?=base_url()?>assets/mokup/mokup.js"></script>
+
 </head>
 
 <body>
-<h1>Accounts Page</h1>
+
+<div class="row mokup-border" style="position:relative;height: 20%; margin: 50px 10% 0 10%;text-align: center;">
+    <div>
+    <span>Account Setting</span>
+    <div class="mokup-border" style="position: absolute;bottom: 0;text-align: center;">
+        <span style="padding: 0 20px;" class="mokup-border">Settings</span>
+        <span style="padding: 0 20px;" class="mokup-border">BAA</span>
+        <span style="padding: 0 20px;" class="mokup-border">Alerts</span>
+        <span style="padding: 0 20px;" class="mokup-border">Upgades</span>
+    </div>
+    </div>
+</div>
+<div class="row mokup-border" style="height: 40%; margin: 50px 10%;text-align: center; padding-top: 50px;position: relative;">
+    <span>Personal information</span><span style="margin-left: 50px;">Edit</span><br><br>
+    <span>Current Plan</span><span style="margin-left: 50px;">Edit(NA,subscription)</span><br><br>
+    <span>Payment history</span><span style="margin-left: 100px;">like to payment history<br><span style="margin-left: 100px;">Credit card info(Edit)</span></span><br><br>
+    <span>Log in Informantion</span><span style="margin-left: 50px;">email/password/changes edit</span><br><br>
+    <span style="position: absolute;bottom: 50px;right: 50px;padding: 50px 5px;" class="mokup-border">Payment history</span>
+</div>
+<div class="row mokup-border" style="height: 40%; margin: 50px 10%;text-align: center;">
+
+</div>
+<div class="row mokup-border" style="height: 40%; margin: 50px 10%;text-align: center;">
+
+</div>
+<div class="row mokup-border" style="height: 40%; margin: 50px 10%;text-align: center;">
+
+</div>
+
+<div class="row mokup-border" style="height: 100px; margin: 0 10% 50px 10%;text-align: center;">
+    footer
+</div>
+
+
 </body>
-</html>
